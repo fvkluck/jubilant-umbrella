840589ed8266a04c21d563c74a41656c1f6f4d5d
FlorianGit <f.v.kluck@gmail.com>
2023-10-07 11:20:12 +0200

    Connections now start anonymous
    Previously, the :neighbours key of a worker would contain a dictionary
    with as key the id of the neighbour. However, in real life, you don't
    know the id of the neighbour until you ask. So now the connections start
    anonymous, and the id of the neighbour is only known after a greeting.

--- src/main.clj
+++ src/main.clj
@@ -58,41 +58,83 @@
       (assoc :in out)
       (assoc :out in)))
 
-(defn add-connection [w conn-id conn]
-  (update w :neighbours assoc conn-id conn))
+(defn add-connection [w conn]
+  (update w :neighbours (fnil conj []) conn))
+
+(defn get-neighbour [w n-id]
+  (->> (:neighbours w)
+       (filter (fn [conn] (= n-id (:id conn))))
+       first))
+
+(defn remove-neighbour [w n-id]
+  (update w :neighbours filter (fn [conn] (not= n-id (:id conn)))))
+
+(defn remove-neighbour-by-in-ch [w ch]
+  (update w :neighbours (fn [neighbours] (filter (fn [neighbour] (not= ch (:in neighbour))) neighbours))))
+
+(defn update-neighbour-by-in-ch [w ch f]
+  (update w :neighbours #(map (fn [n]
+                                (if (= ch (:in n))
+                                  (f n)
+                                  n)) %)))
+
+(rcf/tests
+  (def w (worker {:id :x}))
+  (def conn (make-connection 3))
+  (swap! w add-connection conn)
+  @w
+  (count (:neighbours @w)) := 1
+  #_(swap! w update-neighbour-by-in-ch (:in conn) (fn [n] (assoc n :foo :bar)))
+  (swap! w remove-neighbour-by-in-ch (:in conn))
+  (count (:neighbours @w)) := 0)
 
 (defn connect-workers! [w1 w2 d]
   (let [conn (make-connection d)]
-    (swap! w1 add-connection (:id @w2) conn)
-    (swap! w2 add-connection (:id @w1) (reverse-connection conn))))
+    (swap! w1 add-connection conn)
+    (swap! w2 add-connection (reverse-connection conn))))
 
 (rcf/tests
   (def wx (atom {:id :x}))
   (def wy (atom {:id :y}))
   (connect-workers! wx wy 3)
-  (nil? (:x (:neighbours @wy))) := false
-  (nil? (:y (:neighbours @wx))) := false)
+  (count (:neighbours @wy)) := 1
+  (count (:neighbours @wx)) := 1)
 
 (defmulti handle-message
   "A handle-message should accept a (worker) state and a msg, and return the updated state and a
   list of messages to send to the neighbours of the worker"
-  (fn [_state msg] (:id msg)))
+  (fn [_state msg ch] (:id msg)))
 
 (defmethod handle-message :greet
-  [state {:keys [sender-id] :as msg}]
+  [state {:keys [sender-id] :as msg} ch]
   (log msg)
   (println "Greeting")
-  [state [[sender-id {:id :greet-reply
-                      :sender (:id state)}]]])
+  [(-> state
+       (update-neighbour-by-in-ch ch (fn [n] (assoc n :id sender-id))))
+   [[sender-id {:id :greet-reply
+                :sender (:id state)}]]])
+
+(rcf/tests
+  (def conn (make-connection 3))
+  (def w {:id :x
+          :neighbours [conn]})
+  (let [[new-state msgs] (handle-message w
+                                         {:id :greet
+                                          :sender-id :y}
+                                         (:in conn))]
+    (-> new-state :neighbours first :id) := :y
+    (-> (first msgs) first) := :y
+    (-> (first msgs) second) := {:id :greet-reply
+                                 :sender :x}))
 
 (defmethod handle-message :greet-reply
-  [state {:keys [_sender-id] :as msg}]
+  [state {:keys [_sender-id] :as msg} ch]
   (log msg)
   (println "Greeting reply")
   [state []])
 
 (defmethod handle-message nil
-  [state msg]
+  [state msg ch]
   (log msg)
   (println "Worker" (:id state) "received message:" msg)
   [state []])
@@ -102,19 +144,21 @@
     (loop []
       (when-let [neighbours (:neighbours @worker)]
         (let [in-chans (->> neighbours
-                             vals
                              (map :in))
               [msg ch] (alts! in-chans)]
           (if (= (:id msg) :disconnect)
             (do
-              (println "Closing")
-              (close! ch)
-              (swap! worker update :neighbours dissoc (:sender-id msg)))
+              (println "Closing" ch)
+              (swap! worker update remove-neighbour-by-in-ch ch)
+              (close! ch))
             (do
-              (let [[new-state msgs] (handler @worker msg)]
+              (let [[new-state msgs] (handler @worker msg ch)]
                 (reset! worker new-state)
                 (doseq [[addressee msg] msgs]
-                  (>! (-> neighbours addressee :out) msg)))
+                  (>! (-> @worker
+                          (get-neighbour addressee)
+                          :out)
+                      msg)))
               (recur))))))))
 
 (rcf/tests
@@ -124,22 +168,16 @@
 
 (rcf/tests
   (def test-chan (chan 1))
-  (defn test-handler [state msg]
+  (defn test-handler [state msg ch]
     (>!! test-chan msg)
     [state []])
   (def in (chan))
   (def out (chan))
   (def w (worker {:id :x}))
-  (swap! w add-connection :u {:in in :out out :distance 4})
+  (swap! w add-connection {:in in :out out :distance 4})
   (listen-neighbours! w test-handler)
   (>!! in "Hello")
-  (<!! test-chan) := "Hello"
-
-  (>!! in {:id :disconnect
-           :sender-id :u})
-  (<!! (async/timeout 100))
-  (:neighbours @w) := {}
-  )
+  (<!! test-chan) := "Hello")
 
 (defn build-network [{:keys [nodes] :as _graph}]
   (into {} (for [n nodes]
@@ -160,11 +198,12 @@
 (rcf/tests
   "greet neighbours"
   (clear-log)
+  (def w (:x network))
   (let [w (:x network)]
-    (doseq [[n-id {:keys [in out d]}] (:neighbours @w)]
+    (doseq [{:keys [in out d]} (:neighbours @w)]
       (go (>! out {:id :greet
                    :sender-id :x}))))
-  (<!! (async/timeout 100))  ; TODO figure out how to naturally wait for the previous statement to finish
+  (<!! (async/timeout 300))  ; TODO figure out how to naturally wait for the previous statement to finish
   (->> (show-log)
        (map :id)
        frequencies) := {:greet 4 :greet-reply 4})
@@ -172,7 +211,7 @@
 (comment (let [in (chan)
                out (chan)
                w (-> (worker {:id :x}))]
-           (swap! w add-connection :u {:in in :out out :distance 4})
+           (swap! w add-connection {:in in :out out :distance 4})
            (listen-neighbours! w handle-message)
            (>!! in "Hello")
            @w
